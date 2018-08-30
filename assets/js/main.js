80f2db859bf1f749e55f3c1013489f0ad37df13d
Cláudia Marinho <claudia.m.r.marinho@gmail.com>
2018-08-30 22:11:35 +0100

    this bug is stupid

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,26 +35,6 @@ jQuery(document).ready(function ($) {
     wow.init();
 
 
-
-    /* ---------------------------------------------------------------------
-     Carousel
-     ---------------------------------------------------------------------= */
-
-    $('.main_home_slider').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: false,
-        navText: [
-            "<i class='lnr lnr-chevron-left'></i>",
-            "<i class='lnr lnr-chevron-right'></i>"
-        ],
-        autoplayHoverPause: true
-
-    });
-
 // main-menu-scroll
 
     jQuery(window).scroll(function () {
