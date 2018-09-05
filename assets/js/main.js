1b11d56105c7335d081cf0cf5f3dde2636d9e6cd
Cláudia Marinho <claudia.m.r.marinho@gmail.com>
2018-09-05 22:28:48 +0100

    will not get rid of loader after all
    will try to make stuff load faster thou

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 "use strict";
 jQuery(document).ready(function ($) {
 
+    $(window).load(function () {
+        $(".loaded").fadeOut();
+        $(".preloader").delay(1000).fadeOut("slow");
+    });
+
     /*---------------------------------------------*
      * Mobile menu
      ---------------------------------------------*/
